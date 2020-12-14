5083d426a5b2e856ee6e3d1f8d258553540bb028
allevas <allevas@gmail.com>
2020-12-14 23:12:55 +0300

    adding movement
    move

---
+++ DreamZombieShooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    private Vector3 movement;
+    private Animator anim;
+    private Rigidbody playerRB;
+    private int floorMask;
+    private float camRayLenght = 100f;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Move(horizontalInput, verticalInput);
+        Turning();
+    }
+
+    private void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRB.MovePosition(transform.position + movement);
+    }
+
+    private void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+        if (Physics.Raycast(camRay, out floorHit, camRayLenght, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point = transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRB.MoveRotation(newRotation);
+        }
+    }
+}
